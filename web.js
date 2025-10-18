008716590dd9389c0ae47e3a2c11b2d838775bf1
kushalchaka <kushalchaka@gmail.com>
2025-10-17 23:28:30 -0500

    formatting fixes

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
-
 const ThemePicker = Object.freeze({
     key: "blog-theme",
+
     set_onclick_handlers() {
         document.querySelectorAll(`input[name="${this.key}"]`).forEach((input_element) => {
             input_element.addEventListener("click", () => {
@@ -10,29 +10,29 @@ const ThemePicker = Object.freeze({
             });
         });
     },
+
     restore_theme_on_load() {
-        const user_selected_theme = localStorage.getItem(this.key) || "light";
-        document.querySelector(`input[name="${this.key}"]#${user_selected_theme}`).checked = true;
+        // Change default to 'dark'
+        const user_selected_theme = localStorage.getItem(this.key) || "dark";
+        const radio = document.querySelector(`input[name="${this.key}"]#${user_selected_theme}`);
+        if (radio) radio.checked = true;
         document.documentElement.className = user_selected_theme;
     },
+
     init() {
         this.restore_theme_on_load();
         this.set_onclick_handlers();
     },
 });
 
-document.addEventListener(
-    "DOMContentLoaded",
-    () => {
-        ThemePicker.init();
-        PageContents.init();
-        Toc.init();
-        MegaTags.mark_links();
-        open_all_external_links_in_a_new_tab();
-        document.querySelectorAll("article pre").forEach((pre) => pre.append(create_copy_select_button(pre)));
-        ReorderTags.init();
-        $("divbody").addEventListener("click", close_sidebar);
-        make_images_expand_on_click();
-    },
-    false
-);
+document.addEventListener("DOMContentLoaded", () => {
+    ThemePicker.init();
+    PageContents.init();
+    Toc.init();
+    MegaTags.mark_links();
+    open_all_external_links_in_a_new_tab();
+    document.querySelectorAll("article pre").forEach((pre) => pre.append(create_copy_select_button(pre)));
+    ReorderTags.init();
+    document.getElementById("divbody").addEventListener("click", close_sidebar);
+    make_images_expand_on_click();
+}, false);
